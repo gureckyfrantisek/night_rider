ce23170aba75ac36c81485367adf6843df262d3d
František Gurecký <gurecfra@cvut.cz>
2025-11-23 14:43:54 +0100

    fix: Add space for readability

--- scripts/finish_screen.gd
+++ scripts/finish_screen.gd
@@ -4,9 +4,9 @@ var replay_scene
 
 func _ready():
 	if Times.was_new_best:
-		$VBoxContainer/ColorRect/Label.text = 'NEW RECORD!\nTime: ' + str(snappedf(Times.last_played, 0.001)) + 's'
+		$VBoxContainer/ColorRect/Label.text = 'NEW RECORD!\nTime: ' + str(snappedf(Times.last_played, 0.001)) + ' s'
 	else:
-		$VBoxContainer/ColorRect/Label.text = 'Level Finished!\nTime: ' + str(snappedf(Times.last_played, 0.001)) + 's'
+		$VBoxContainer/ColorRect/Label.text = 'Level Finished!\nTime: ' + str(snappedf(Times.last_played, 0.001)) + ' s'
 
 
 func _on_restart_pressed():
--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -14,7 +14,7 @@ func _on_level_1_mouse_entered():
 	if Times.level1 == null:
 		$Levels/LevelsTop/Level1.text = 'Level 1\nNo time yet\nPlay'
 	else:
-		$Levels/LevelsTop/Level1.text = 'Level 1\nTime: ' + str(snappedf(Times.level1, time_precission)) + 's\nPlay'
+		$Levels/LevelsTop/Level1.text = 'Level 1\nTime: ' + str(snappedf(Times.level1, time_precission)) + ' s\nPlay'
 
 
 func _on_level_1_mouse_exited():
@@ -29,7 +29,7 @@ func _on_level_2_mouse_entered():
 	if Times.level2 == null:
 		$Levels/LevelsTop/Level2.text = 'Level 2\nNo time yet\nPlay'
 	else:
-		$Levels/LevelsTop/Level2.text = 'Level 2\nTime: ' + str(snappedf(Times.level2, time_precission)) + 's\nPlay'
+		$Levels/LevelsTop/Level2.text = 'Level 2\nTime: ' + str(snappedf(Times.level2, time_precission)) + ' s\nPlay'
 
 func _on_level_2_mouse_exited():
 	$Levels/LevelsTop/Level2.text = 'Level 2'
@@ -43,7 +43,7 @@ func _on_level_3_mouse_entered() -> void:
 	if Times.level3 == null:
 		$Levels/LevelsTop/Level3.text = 'Level 3\nNo time yet\nPlay'
 	else:
-		$Levels/LevelsTop/Level3.text = 'Level 3\nTime: ' + str(snappedf(Times.level3, time_precission)) + 's\nPlay'
+		$Levels/LevelsTop/Level3.text = 'Level 3\nTime: ' + str(snappedf(Times.level3, time_precission)) + ' s\nPlay'
 
 
 func _on_level_3_mouse_exited() -> void:
@@ -57,7 +57,7 @@ func _on_level_4_mouse_entered() -> void:
 	if Times.level4 == null:
 		$Levels/LevelsTop/Level4.text = 'Level 4\nNo time yet\nPlay'
 	else:
-		$Levels/LevelsTop/Level4.text = 'Level 4\nTime: ' + str(snappedf(Times.level4, time_precission)) + 's\nPlay'
+		$Levels/LevelsTop/Level4.text = 'Level 4\nTime: ' + str(snappedf(Times.level4, time_precission)) + ' s\nPlay'
 
 
 func _on_level_4_mouse_exited() -> void:
@@ -70,7 +70,7 @@ func _on_level_5_mouse_entered() -> void:
 	if Times.level5 == null:
 		$Levels/LevelsTop/Level5.text = 'Level 5\nNo time yet\nPlay'
 	else:
-		$Levels/LevelsTop/Level5.text = 'Level 5\nTime: ' + str(snappedf(Times.level5, time_precission)) + 's\nPlay'
+		$Levels/LevelsTop/Level5.text = 'Level 5\nTime: ' + str(snappedf(Times.level5, time_precission)) + ' s\nPlay'
 
 
 func _on_level_5_mouse_exited() -> void:
@@ -83,7 +83,7 @@ func _on_level_6_mouse_entered() -> void:
 	if Times.level6 == null:
 		$Levels/LevelsBot/Level6.text = 'Level 6\nNo time yet\nPlay'
 	else:
-		$Levels/LevelsBot/Level6.text = 'Level 6\nTime: ' + str(snappedf(Times.level6, time_precission)) + 's\nPlay'
+		$Levels/LevelsBot/Level6.text = 'Level 6\nTime: ' + str(snappedf(Times.level6, time_precission)) + ' s\nPlay'
 
 func _on_level_6_mouse_exited() -> void:
 	$Levels/LevelsBot/Level6.text = 'Level 6'
@@ -95,7 +95,7 @@ func _on_level_7_mouse_entered() -> void:
 	if Times.level7 == null:
 		$Levels/LevelsBot/Level7.text = 'Level 7\nNo time yet\nPlay'
 	else:
-		$Levels/LevelsBot/Level7.text = 'Level 7\nTime: ' + str(snappedf(Times.level7, time_precission)) + 's\nPlay'
+		$Levels/LevelsBot/Level7.text = 'Level 7\nTime: ' + str(snappedf(Times.level7, time_precission)) + ' s\nPlay'
 
 func _on_level_7_mouse_exited() -> void:
 	$Levels/LevelsBot/Level7.text = 'Level 7'
@@ -107,7 +107,7 @@ func _on_level_8_mouse_entered() -> void:
 	if Times.level8 == null:
 		$Levels/LevelsBot/Level8.text = 'Level 8\nNo time yet\nPlay'
 	else:
-		$Levels/LevelsBot/Level8.text = 'Level 8\nTime: ' + str(snappedf(Times.level8, time_precission)) + 's\nPlay'
+		$Levels/LevelsBot/Level8.text = 'Level 8\nTime: ' + str(snappedf(Times.level8, time_precission)) + ' s\nPlay'
 
 func _on_level_8_mouse_exited() -> void:
 	$Levels/LevelsBot/Level8.text = 'Level 8'
@@ -119,7 +119,7 @@ func _on_level_9_mouse_entered() -> void:
 	if Times.level9 == null:
 		$Levels/LevelsBot/Level9.text = 'Level 9\nNo time yet\nPlay'
 	else:
-		$Levels/LevelsBot/Level9.text = 'Level 9\nTime: ' + str(snappedf(Times.level9, time_precission)) + 's\nPlay'
+		$Levels/LevelsBot/Level9.text = 'Level 9\nTime: ' + str(snappedf(Times.level9, time_precission)) + ' s\nPlay'
 
 func _on_level_9_mouse_exited() -> void:
 	$Levels/LevelsBot/Level9.text = 'Level 9'
@@ -131,7 +131,7 @@ func _on_level_10_mouse_entered() -> void:
 	if Times.level10 == null:
 		$Levels/LevelsBot/Level10.text = 'Level 10\nNo time yet\nPlay'
 	else:
-		$Levels/LevelsBot/Level10.text = 'Level 10\nTime: ' + str(snappedf(Times.level10, time_precission)) + 's\nPlay'
+		$Levels/LevelsBot/Level10.text = 'Level 10\nTime: ' + str(snappedf(Times.level10, time_precission)) + ' s\nPlay'
 
 func _on_level_10_mouse_exited() -> void:
 	$Levels/LevelsBot/Level10.text = 'Level 10'
@@ -152,7 +152,7 @@ func _on_bonus_mouse_entered() -> void:
 	if Times.bonus == null:
 		$Levels/BONUS/Bonus.text = 'BONUS\nNo time yet\nPlay'
 	else:
-		$Levels/BONUS/Bonus.text = 'BONUS\nTime: ' + str(snappedf(Times.bonus, time_precission)) + 's\nPlay'
+		$Levels/BONUS/Bonus.text = 'BONUS\nTime: ' + str(snappedf(Times.bonus, time_precission)) + ' s\nPlay'
 
 
 func _on_bonus_mouse_exited() -> void:
